9e6d380b8e07e7861966e2acd87a0b3a180a49d0
Stefano Ordine <stefano.ordine@gmail.com>
2015-01-02 13:51:13 +0100

    Restyle landing.page

--- assets/javascripts/layout.js
+++ assets/javascripts/layout.js
@@ -20,4 +20,19 @@ $(document).ready(function() {
     },
     minLength: 2
   });
+
+  // Scrolls to the selected menu item on the page
+  $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+              $('html,body').animate({
+                  scrollTop: target.offset().top
+              }, 500);
+              return false;
+          }
+      }
+  });
 });
