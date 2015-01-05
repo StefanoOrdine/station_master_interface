9901029c39ef24b91507f417417c7455eeab764b
Stefano Ordine <stefano.ordine@gmail.com>
2015-01-05 14:58:34 +0100

    Argh!

--- lib/services/schedule.rb
+++ lib/services/schedule.rb
@@ -12,7 +12,7 @@ module Services
 
       def call!
         StationMaster::Schedule.find_station_departures(station_code)
-          .select { |schedule| schedule.time >= Time.now.in_time_zone('settings.time_zone') }
+          .select { |schedule| schedule.time >= Time.now.in_time_zone(settings.time_zone) }
           .sort!{ |a, b| a.time <=> b.time }
           .first(entries_count)
       end
@@ -30,7 +30,7 @@ module Services
 
       def call!
         StationMaster::Schedule.find_station_arrivals(station_code)
-          .select { |schedule| schedule.time >= Time.now.in_time_zone('settings.time_zone') }
+          .select { |schedule| schedule.time >= Time.now.in_time_zone(settings.time_zone) }
           .sort!{ |a, b| a.time <=> b.time }
           .first(entries_count)
       end
